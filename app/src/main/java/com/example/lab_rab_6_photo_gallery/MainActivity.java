3592115db1479739a4b6fd07737d7cad4dc99240
Your Name <you@example.com>
2020-12-21 23:56:19 +0700

    vers1

---
+++ app/src/main/java/com/example/lab_rab_6_photo_gallery/MainActivity.java
@@ -0,0 +1,14 @@
+Lab_rab_6_Photo_Gallerypackage com.example.lab_rab_6_photo_gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
